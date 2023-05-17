87c404f61951b8e9c43fe9bee3c906b5f6cbed4e
antonioam82 <32330632+antonioam82@users.noreply.github.com>
2023-05-17 19:13:45 +0200

    Update chatcmd.py

--- chatcmd.py
+++ chatcmd.py
@@ -1,65 +1,40 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import openai
-import time
-import os
-import sys
-from colorama import Fore, init, Style
 import argparse
+import time
+from colorama import Fore, init
+import openai
 
 init()
 
-def main():
-    parser = argparse.ArgumentParser(prog="ChatCMD",conflict_handler='resolve',
-                                     epilog="REPO: https://github.com/antonioam82/ChatPy")
-    parser.add_argument('-ap','--api_key',required=True,type=str,help="Enter ChatGPT API KEY")
-    parser.add_argument('-mt','--max_tokens',type=int,default=2048,help="Enter max tokens")
-    parser.add_argument('-eng','--engine',type=str,default="text-davinci-003",help="Model to use")
-
-    args = parser.parse_args()
-
-    try:
-        openai.api_key = args.api_key
-        model_lst = openai.Model.list()
-        model_find = False
-        for i in model_lst['data']:
-            if i['id'] == args.engine:
-                model_find = True
-                break
-        if model_find == False:
-            parser.error(Fore.RED+Style.BRIGHT+f"BAD MODEL SELECTED: {args.engine} not valid"+Fore.RESET+Style.RESET_ALL)
-    except Exception as e:
-        parser.error(Fore.RED+Style.BRIGHT+str(e)+Fore.RESET+Style.RESET_ALL)
-        
-        
-
-    chat(args)
+def setup_openai(api_key, engine):
+    openai.api_key = api_key
+    model_lst = openai.Model.list()
+    if engine not in [i['id'] for i in model_lst['data']]:
+        raise ValueError(Fore.RED + f"BAD MODEL SELECTED: {engine} not valid" + Fore.RESET)
+    return engine
 
 def typewriter(message):
     print(Fore.GREEN)
-    for i in message:
-        sys.stdout.write(i)
-        sys.stdout.flush()
+    for char in message:
+        print(char, end='', flush=True)
         time.sleep(0.01)
     print(Fore.RESET)
 
-def get_completion(args,p):
+def get_completion(engine, prompt, max_tokens):
     try:
-        completion = openai.Completion.create(engine=args.engine,
-                                              prompt = p,
-                                              max_tokens=args.max_tokens)
+        completion = openai.Completion.create(engine=engine, prompt=prompt, max_tokens=max_tokens)
         return str(completion.choices[0].text)
     except Exception as e:
-        print(Fore.RED+Style.BRIGHT+str(e)+Fore.RESET+Style.RESET_ALL)
-        
+        print(Fore.RED + str(e) + Fore.RESET)
 
-def save_response(r):
+def save_response(response):
     try:
-        with open("response.txt","w") as document:
-            document.write(r)
-        print(Fore.YELLOW+"Saved document as 'response.txt'."+Fore.RESET)
+        with open("response.txt", "w") as document:
+            document.write(response)
+        print(Fore.YELLOW + "Saved document as 'response.txt'." + Fore.RESET)
     except Exception as e:
-        print(Fore.RED+Style.BRIGHT+str(e)+Fore.RESET+Style.RESET_ALL)
+        print(Fore.RED + str(e) + Fore.RESET)
 
 def print_title():
     print(Fore.BLUE)
@@ -70,15 +45,8 @@ def print_title():
     print("                                      \____|_| |_|\__,_|\__|\____|_|  |_|____/ ")
     print("                                       C  O  M  P  U  T  E  R    T  A  L  K  S")
     print(Fore.RESET)
-    
 
-'''def set_api_key(val):
-    try:
-        openai.api_key = val
-    except Exception as e:
-        raise argparse.ArgumentTypeError(Fore.RED+Style.BRIGHT+str(e)+Fore.RESET+Style.RESET_ALL)'''
-
-def chat(args):
+def chat(api_key, engine, max_tokens):
     response = ""
     print_title()
     while True:
@@ -87,17 +55,32 @@ def chat(args):
         if prompt == "END":
             break
         elif prompt == "PRINT":
-            if response != "":
+            if response:
                 save_response(response)
             else:
-                print(Fore.RED+Style.BRIGHT+"No response to print"+Fore.RESET+Style.RESET_ALL)
+                print(Fore.RED + "No response to print" + Fore.RESET)
         elif prompt == "":
             pass
-
         else:
-            response = get_completion(args,prompt)
+            response = get_completion(engine, prompt, max_tokens)
             typewriter(response)
 
-if __name__=='__main__':
+def main():
+    parser = argparse.ArgumentParser(prog="ChatCMD", conflict_handler='resolve',
+                                     epilog="REPO: https://github.com/antonioam82/ChatPy")
+    parser.add_argument('-ap', '--api_key', required=True, type=str, help="Enter ChatGPT API KEY")
+    parser.add_argument('-mt', '--max_tokens', type=int, default=2048, help="Enter max tokens")
+    parser.add_argument('-eng', '--engine', type=str, default="text-davinci-003", help="Model to use")
+
+    args = parser.parse_args()
+
+    try:
+        engine = setup_openai(args.api_key, args.engine)
+    except Exception as e:
+        parser.error(Fore.RED + str(e) + Fore.RESET)
+
+    chat(args.api_key, engine, args.max_tokens)
+
+if __name__ == '__main__':
     main()
 
